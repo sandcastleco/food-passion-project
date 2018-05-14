b7d3544ee3af3d4131ddabf7fd95e5bdba0deaff
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-05-14 12:38:42 -0500

    Add logo

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,5 @@
 <nav class="pa3 tc" role="navigation">
-  <a class="dib pa3" href="/"><?php bloginfo('name'); ?></a>
+  <a class="dib pa3" href="/"><img class="dib w3 v-mid" alt="<?php bloginfo('name'); ?>" src="<?php get_image_uri('food-passion-project-logo.svg'); ?>"></a>
   <?php
     wp_nav_menu( array(
       'menu' => 'main-menu',
@@ -8,7 +8,7 @@
       'container_id' => FALSE,
       'menu_class' => 'dib list pl0 mv0',
       'depth' => 1,
-      'walker' => new Nav_Walker(array('dib', 'dib pa2 ink link ttu f5'))
+      'walker' => new Nav_Walker(array('dib', 'dib pa2 pa3-l ink link ttu f5'))
       )
     );
   ?>
