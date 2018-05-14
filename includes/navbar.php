4a41670c811f0ea5b395b424112ef0558b918fa4
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-05-14 12:43:50 -0500

    Set up feature menu

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,4 +12,16 @@
       )
     );
   ?>
+  <?php
+    wp_nav_menu( array(
+      'menu' => 'feature-menu',
+      'theme_location' => 'feature-menu',
+      'container' => FALSE,
+      'container_id' => FALSE,
+      'menu_class' => 'dib list pl0 mv0',
+      'depth' => 1,
+      'walker' => new Nav_Walker(array('dib', 'dib pa2 pa3-l compote link ttu f5'))
+      )
+    );
+  ?>
 </nav>
--- modules/menus.php
+++ modules/menus.php
@@ -3,7 +3,8 @@
 function register_menus() {
 	register_nav_menus(
 		array(
-		 'main-menu' => __( 'Main Menu' )
+		 'main-menu' => __( 'Main Menu' ),
+		 'feature-menu' => __( 'Feature Menu' )
 		)
 	);
 }
