e7f7e7e261b2bb1c56974238c87cd4f3daed7373
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-05-23 14:25:06 -0500

    Set up basic blog template

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
 <?php get_header(); ?>
 
-<section class="pa4">
-  <h1>The blog is here</h1>
-  <h2><?php single_cat_title(); ?></h2>
-  <?php the_archive_description(); ?>
+<?php if (has_post_thumbnail( $post->ID ) ): ?>
+  <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
+  <section class="ph3 pv5 cover" style="background-image: url(<?php echo $image[0]; ?>);">
+<?php else: ?>
+  <section class="ph3 pv5 cover" style="background-image: url(<?php get_image_uri('background-2.jpg'); ?>);">
+<?php endif; ?>
+
+  <div class="mw7 center tc">
+    <h1 class="dib mt5 mb3 f1 f-subheadline-l compote ttu lh-solid fw9 tr">Food <br>passion <br>project <br><span class="white">blog</span></h1>
+  </div>
+</section>
+
+<section class="ph3 pv5 shadow-2 z-max relative" style="background-image: url(<?php get_image_uri('background-2.jpg'); ?>);">
+  <div class="pa4 mw8 center cf bg-white-80">
+    <div class="mw6 center">
+      <p>Categories list sidebar?</p>
+      <h2><?php single_cat_title(); ?></h2>
+      <?php the_archive_description(); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="mb5 mt3">
+          <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+          <?php if (has_post_thumbnail(get_the_id())) : ?>
+            <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
+          <?php endif; ?>
+          <?php the_excerpt(); ?>
+        </div>
+      <?php endwhile; else: ?>
+        <article>
+          <p><?php _e('Blog coming soon!'); ?></p>
+        </article>
+      <?php endif; ?>
+    </div>
+  </div>
 </section>
 
 <?php get_footer(); ?>
