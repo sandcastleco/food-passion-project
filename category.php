87fe5b3a67dcf8f427d75e98d4cba486983f9a4a
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-07-02 12:07:19 -0500

    Adjust blog index, category, and post styling

--- category.php
+++ category.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<?php if (has_post_thumbnail( $page->ID ) ): ?>
+<?php if (has_post_thumbnail( $post->ID ) ): ?>
   <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id(get_option('page_for_posts')),'full'); ?>
   <section class="ph3 pv5 cover" style="background-image: url(<?php echo $image[0]; ?>);">
 <?php else: ?>
@@ -8,7 +8,8 @@
 <?php endif; ?>
 
   <div class="jumbotron mw7 center tc">
-    <h1 class="jumbotron-title dib mt5 pt4 pt0-ns mb3 f1 f-subheadline-l compote ttu lh-solid fw9 tr">Food <br>passion <br>project <br><span class="jumbotron-highlight white">blog</span></h1>
+    <h1 class="jumbotron-title dib mt5 pt4 pt0-ns mb3 f1 f-subheadline-l compote ttu lh-solid fw9 tr">Food <br>passion <br>project <br><span class="jumbotron-highlight white"><?php single_cat_title(); ?></span></h1>
+    <div class="f3 measure-narrow center"><?php the_archive_description(); ?></div>
   </div>
 </section>
 
@@ -16,9 +17,8 @@
   <div class="pa4 mw8 center cf bg-white-80">
     <div class="mw6 center">
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <div class="mb5 mt3 pa3 bg-white">
-          <h2 class="mv0"><a class="link compote" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-          <span class="dib mb2"><?php echo get_the_time('F jS, Y'); ?></span>
+        <div class="mb5 mt3 pa3 bg-white br b--compote bw2">
+          <h2 class="mt0"><a class="link compote" href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
           <?php if (has_post_thumbnail(get_the_id())) : ?>
             <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
           <?php endif; ?>
---
+++ single.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<?php if (has_post_thumbnail(get_the_id())) : ?>
+  <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
+  <section class="ph3 pv5 cover" style="background-image: url(<?php echo $image[0]; ?>);">
+<?php else: ?>
+  <section class="ph3 pv5 cover" style="background-image: url(<?php get_image_uri('background-2.jpg'); ?>);">
+<?php endif; ?>
+
+  <div class="jumbotron mv5 mw6 center tc">
+    <h1 class="jumbotron-title dib mt5 mv0-ns pv3 ph4 mb3 f1 compote ttu lh-title fw9 bg-white-80"><?php the_title(); ?></h1>
+  </div>
+</section>
+
+<section class="ph3 pv5 shadow-2 z-max relative" style="background-image: url(<?php get_image_uri('background-2.jpg'); ?>);">
+  <div class="pa4 mw8 center cf bg-white-80">
+    <div class="mw6 center">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="mb5 mt3">
+          <?php the_content(); ?>
+        </div>
+        <hr class="b--compote mb5 w-50">
+        <div>
+          <img alt="<?php echo get_the_author_meta('display_name'); ?>" class="fr br-pill ml3 mw4" src="<?php echo get_avatar_url(get_the_author_meta('ID'), array('size'=>256)); ?>">
+          <p><?php echo get_the_author_meta('description'); ?></p>
+        </div>
+
+        <hr class="b--compote mv5 w-50">
+
+        <p class="tc">Published <?php echo get_the_time('F jS, Y'); ?></p>
+      <?php endwhile; endif; ?>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
